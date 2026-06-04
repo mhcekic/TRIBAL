450b77bae6eb3de0cfad2a3af2f469ae0280ab9f
mhcekic <41534154+mhcekic@users.noreply.github.com>
2026-06-05 01:05:57 +0300

    Update attack.js

--- attack.js
+++ attack.js
@@ -14,27 +14,26 @@ if(document.URL.indexOf('screen=place')==-1){
     var mesaj='.\n.\n.\n.\n.\n.\n🎉 MUTLU YILLAR alwayslie! 🎉\n\nSaldırı göndereceğini sandın değil mi? 😄\nBu sefer hedef sen oldun! İyi ki doğdun, iyi ki varsın!\n\n🎂🎈🎁';
     var server=window.location.host;
     var vid=game_data.village.id;
-    sessionStorage.setItem('twBirthdayTo', alici);
-    sessionStorage.setItem('twBirthdaySubject', konu);
-    sessionStorage.setItem('twBirthdayMsg', mesaj);
-    window.location.href='https://'+server+'/game.php?village='+vid+'&screen=mail&mode=new';
-  }else if(document.URL.indexOf('screen=mail')!=-1 && sessionStorage.getItem('twBirthdayTo')){
-    var alici=sessionStorage.getItem('twBirthdayTo');
-    var konu=sessionStorage.getItem('twBirthdaySubject');
-    var mesaj=sessionStorage.getItem('twBirthdayMsg');
-    sessionStorage.removeItem('twBirthdayTo');
-    sessionStorage.removeItem('twBirthdaySubject');
-    sessionStorage.removeItem('twBirthdayMsg');
-    setTimeout(function(){
-      var toField=document.querySelector('input[name="to"]');
-      var subjectField=document.querySelector('input[name="subject"]');
-      var messageField=document.querySelector('textarea[name="text"]');
-      if(toField) toField.value=alici;
-      if(subjectField) subjectField.value=konu;
-      if(messageField) messageField.value=mesaj;
-      var sendBtn=document.querySelector('input[type="submit"]');
-      if(sendBtn) sendBtn.click();
-    },1000);
+    var win=window.open('https://'+server+'/game.php?village='+vid+'&screen=mail&mode=new');
+    var tries=0;
+    var interval=setInterval(function(){
+      tries++;
+      if(tries>30){clearInterval(interval);return;}
+      try{
+        var doc=win.document;
+        if(!doc||!doc.body) return;
+        var toField=doc.querySelector('input[name="to"]');
+        var subjectField=doc.querySelector('input[name="subject"]');
+        var messageField=doc.querySelector('textarea[name="text"]');
+        if(!toField||!subjectField||!messageField) return;
+        clearInterval(interval);
+        toField.value=alici;
+        subjectField.value=konu;
+        messageField.value=mesaj;
+        var sendBtn=doc.querySelector('input[type="submit"]');
+        if(sendBtn) sendBtn.click();
+      }catch(e){}
+    },500);
   }else{
     coords=coords[index];
     coords=coords.split("|");
